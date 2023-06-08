60959ffa18c750506434e9f8275ed689e4a299a5
shaunramsey <shaun.ramsey@gmail.com>
2023-06-08 12:57:38 -0400

    app updates

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
+import { React } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { FACircle } from "FACircle";
 
+
 const Stack = createStackNavigator();
 
-const AppStack = () => {
-    return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="FACircle" component={FACircle} />
-        </Stack.Navigator>
-    );
-}
-
-
-const App = () => {
+export default function App() {
     return (
         < NavigationContainer >
-            <Stack />
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="FACircle" component={FACircle} />
+            </Stack.Navigator>1
         </NavigationContainer >
     );
 }
