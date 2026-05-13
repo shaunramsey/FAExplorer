86f64cf8d9f0745c390a3cd9590fa6e73e57b805
Shaun Ramsey <shaun.ramsey@gmail.com>
2026-05-13 11:43:09 -0400

    new base href test

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2002730639" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "541294559" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
