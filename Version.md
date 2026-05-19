eb6bbec9714abb607b0dbe5e9578552c199463a1
Shaun Ramsey <shaun.ramsey@gmail.com>
2026-05-19 12:01:34 -0400

    - verison file update to makeweb - most recent version pushed

--- Version.md
+++ Version.md
@@ -1,5 +1,4 @@
-# Status of Finite Automata Designer
+# Build Status of Finite Automata Designer
 * Generated on: **2026-05-19** at **12:00:30**
 * Build number **31**
 * [Go Back](README.md) - go to the Readme
-* [Latest Build](build/web/index.html) - go to the last build
--- makeweb.sh
+++ makeweb.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Define the target file name
-OUTPUT_FILE="status.md"
+OUTPUT_FILE="Status.md"
+VERSION_FILE="Version.md"
 
 # Format the date and time strings
 CURRENT_DATE=$(date "+%Y-%m-%d")
@@ -11,6 +12,8 @@ CURRENT_TIME=$(date "+%H:%M:%S")
 echo "# Status of Finite Automata Designer"  > "$OUTPUT_FILE"
 echo "* Generated on: **$CURRENT_DATE** at **$CURRENT_TIME**" >> "$OUTPUT_FILE"
 
+echo "# Build Status of Finite Automata Designer"  > "$VERSION_FILE"
+echo "* Generated on: **$CURRENT_DATE** at **$CURRENT_TIME**" >> "$VERSION_FILE"
 
 FILE="counter.txt"
 
@@ -22,9 +25,13 @@ NEW_NUM=$((NUM + 1))
 
 # Write back to file
 echo "$NEW_NUM" > "$FILE"
+
 echo "* Build number **$NEW_NUM**" >> "$OUTPUT_FILE"
 echo "* [Go Back](README.md) - go to the Readme"  >> "$OUTPUT_FILE"
 echo "* [Latest Build](build/web/index.html) - go to the last build"  >> "$OUTPUT_FILE"
+
+echo "* Build number **$NEW_NUM**" >> "$VERSION_FILE"
+echo "* [Go Back](README.md) - go to the Readme"  >> "$VERSION_FILE"
 
 
 if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
@@ -33,4 +40,4 @@ else
     flutter build web --release --base-href=/FAExplorer/build/web/
 fi
 
-echo "Output successfully written to $OUTPUT_FILE"
+echo "Output successfully written to $OUTPUT_FILE and $VERSION_FILE"
